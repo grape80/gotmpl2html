dc2df2b1a8359bbc747d0e6214fcedefda557af8
hachirog <hachiro@grape80.com>
2023-05-03 03:19:43 +0000

    Create files.

---
+++ cmd/gotmpl2html/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/grape80/gotmpl2html"
+)
+
+func main() {
+	fmt.Print(gotmpl2html.Usage)
+}
---
+++ usage.go
@@ -0,0 +1,28 @@
+package gotmpl2html
+
+const Usage string = `
+Usage:
+    gotmpl2html [commands] [options] <argument>
+
+Description:
+    gotmpl2html is a cli tool to convert gotemplate to html.
+
+Commands:
+    version    Print version.
+    help       Print usage.
+
+Options:
+    --basedir <directory>    Specify the base directory of the root relative path.
+
+Argument:
+    Specify a gotemplate file.
+
+Examples:
+    gotmpl2html input.gotmpl > output.html
+    gotmpl2html version
+    gotmpl2html help
+
+Authors:
+    [hachirog](https://github.com/hachirog)
+
+`
