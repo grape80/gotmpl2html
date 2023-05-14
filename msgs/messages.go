52b399397489565043b0dfbd6261b970355916d0
hachirog <hachiro@grape80.com>
2023-05-14 04:28:05 +0000

    Create file.

---
+++ msgs/messages.go
@@ -0,0 +1,8 @@
+package msgs
+
+const (
+	ERR_UNEXPECTED    = "An unexpected error has occurred."
+	ERR_NO_ARGS       = "No arguments specified."
+	ERR_NO_GOTMPL     = "No gotemplate-file specified."
+	ERR_MULTI_GOTMPLS = "Multiple gotemplate-files specified."
+)
