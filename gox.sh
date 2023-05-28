e4dd651922c6e8756fe04cf45bf2b25674387e20
hachirog <hachiro@grape80.com>
2023-05-28 12:40:13 +0000

    Create file.

---
+++ gox.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+. .build/.env
+
+app=$APP_NAME
+version=$VERSION
+mainDir=$MAIN_DIR
+distDir=$DIST_DIR
+
+distlist=.build/godist.list
+gobuild_opts=$(cat .build/gobuild.opts | tr '\n' ' ')
+
+for target in $(cat $distlist | grep -v '#')
+do
+    os=$(echo $target | cut -d '/' -f 1)
+    arch=$(echo $target | cut -d '/' -f 2)
+
+    if [ $os = "windows" ]; then
+        CGO_ENABLED=0 GOOS=$os GOARCH=$arch eval go build $gobuild_opts -o $distDir/$os-$arch/$app.exe ./$mainDir && \
+        zip -j $distDir/$app-$version.$os-$arch.zip $distDir/$os-$arch/$app.exe
+    else
+        CGO_ENABLED=0 GOOS=$os GOARCH=$arch eval go build $gobuild_opts -o $distDir/$os-$arch/$app ./$mainDir && \
+        tar zcvf $distDir/$app-$version.$os-$arch.tar.gz -C $distDir/$os-$arch $app
+    fi
+done
